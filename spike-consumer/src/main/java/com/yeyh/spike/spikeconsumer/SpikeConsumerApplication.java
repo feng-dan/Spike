e2453b8a124f5ae502a93ade0e64ff3e63f409c3
yeyh <1113531030@qq.com>
2019-07-15 17:10:33 +0800

    第一次提交

---
+++ spike-consumer/src/main/java/com/yeyh/spike/spikeconsumer/SpikeConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.yeyh.spike.spikeconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpikeConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpikeConsumerApplication.class, args);
+	}
+
+}
---
+++ spike-provider/src/main/java/com/yeyh/spike/spikeprovider/SpikeProviderApplication.java
@@ -0,0 +1,13 @@
+package com.yeyh.spike.spikeprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpikeProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpikeProviderApplication.class, args);
+	}
+
+}
